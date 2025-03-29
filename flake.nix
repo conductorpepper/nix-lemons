9253207fb00c61c74f13f8e0397df63227cfeb30
conductorpepper <100497642+conductorpepper@users.noreply.github.com>
2025-03-29 17:12:37 -0400

    init flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "pepper's nix packages";
+
+  outputs = {nixpkgs, ...}: let
+    pkgsOf = system: (import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    });
+  in {
+    devShells.x86_64-linux.default = let
+      pkgs = pkgsOf "x86_64-linux";
+    in {
+      packages = with pkgs; [
+        nil
+        alejandra
+      ];
+    };
+  };
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+}
